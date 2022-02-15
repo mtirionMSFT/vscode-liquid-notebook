b1c7985f7227bba3490489e2c6dd51384291f40b
Martin Tirion <mtirion@live.nl>
2022-02-15 17:31:14 +0100

    Moved build script outside of extension. Build win & linux now.

--- src/extension/scripts/build.ps1
+++
@@ -1,5 +0,0 @@
-$project = "../LiquidParser"
-$outputDirectory = "dist" # dist for publishing, out for development
-$outFolder = (New-Item -Name "$outputDirectory/LiquidParser" -ItemType Directory -Force).ToString();
-
-dotnet build $project -o $outFolder -c Release

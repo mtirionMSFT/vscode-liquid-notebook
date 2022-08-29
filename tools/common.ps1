c3e423858996ed84419ac9bf7f2d86cbd47ff042
Martin Tirion <mtirion@live.nl>
2022-08-29 14:55:32 +0200

    Bump version to 1.0.0

--- tools/common.ps1
+++ tools/common.ps1
@@ -1,7 +1,10 @@
 function UpdatePackageJson {
     param($packagePath, $version)
     $packageJson = Get-Content $packagePath -Raw
-    $packageJson = [Regex]::Replace($packageJson, '("version"\s+:\s+"[\d.]+")', '"version": "$version"')
+    $packageJson = [Regex]::Replace($packageJson, '("version":\s+".+")', """version"": ""$($version)""")
+    Write-Host "HERE IS THE PACKAGE JSON:"
+    Write-Host $packageJson
+    Write-Host "++++++++++++++++++++"
     [System.IO.File]::WriteAllLines($packagePath, $packageJson)
-    #$packageJson | Set-Content $packagePath -Force -Encoding UTF8
+    #$packageJson | Set-Content $packagePath -Force -Encoding UTF8 ==> OUTPUT AS BOM GIVES ERROR
 }

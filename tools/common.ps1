99a96e04077220be0e864ebae5d7ef699249eab5
Martin Tirion <mtirion@live.nl>
2022-08-29 14:58:14 +0200

    Fixed output

--- tools/common.ps1
+++ tools/common.ps1
@@ -2,9 +2,6 @@ function UpdatePackageJson {
     param($packagePath, $version)
     $packageJson = Get-Content $packagePath -Raw
     $packageJson = [Regex]::Replace($packageJson, '("version":\s+".+")', """version"": ""$($version)""")
-    Write-Host "HERE IS THE PACKAGE JSON:"
-    Write-Host $packageJson
-    Write-Host "++++++++++++++++++++"
     [System.IO.File]::WriteAllLines($packagePath, $packageJson)
     #$packageJson | Set-Content $packagePath -Force -Encoding UTF8 ==> OUTPUT AS BOM GIVES ERROR
 }
